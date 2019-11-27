18918de4aca9134edb8601c40da2f905550e5490
ling <i@e7iling.com>
2019-11-27 21:43:01 +0800

    开始工程~

--- t.php
+++ t.php
@@ -8,4 +8,14 @@
 
 require_once 'AliDysms.php';
 
+$cfg = json_decode(file_get_contents('.cfg'), true);
+
+try {
+    $sms = new AliDysms($cfg['accessKeyId'], $cfg['accessKeySecret']);
+
+
+} catch (Exception $exception) {
+
+}
+
 
